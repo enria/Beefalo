ee3f8ce1316b8a0183aa9a10eaeb2a8cbab821fb
zhangyd <1397280422@qq.com>
2020-10-02 20:29:31 +0800

    解决设置界面的适配问题

--- plugins/setting/setting.py
+++ plugins/setting/setting.py
@@ -6,7 +6,7 @@ from PyQt5 import QtGui, QtCore
 from PyQt5.QtCore import QSize, QModelIndex, Qt
 from PyQt5.QtGui import QIcon, QFont, QGuiApplication, QPixmap, QCursor
 from PyQt5.QtWidgets import QDialog, QTextEdit, QVBoxLayout, QDesktopWidget, QHBoxLayout, QListWidget, QListWidgetItem, \
-    QWidget, QLabel, QGroupBox, QPushButton, QSpacerItem, QSizePolicy, QPlainTextEdit
+    QWidget, QLabel, QGroupBox, QPushButton, QSpacerItem, QSizePolicy, QPlainTextEdit, QApplication
 from plugin_api import AbstractPlugin, ContextApi, PluginInfo, SettingInterface, get_logger
 from result_model import ResultItem, ResultAction, MenuItem
 
@@ -14,51 +14,45 @@ log = get_logger("Setting")
 
 
 class PluginEditorWidget(QWidget):
-    def __init__(self, plugin: SettingInterface and AbstractPlugin):
+    def __init__(self, plugin: SettingInterface and AbstractPlugin, api: ContextApi):
         super(QWidget, self).__init__()
         self.plugin = plugin
-        vly = QVBoxLayout()
-        vly.setContentsMargins(10, 10, 0, 10)
-        self.setLayout(vly)
-        label = QLabel(plugin.meta_info.name)
-        label.setFixedHeight(48)
+        self.api = api
+        self.vly = QVBoxLayout()
+        self.setLayout(self.vly)
+        self.label = QLabel(plugin.meta_info.name)
         font = QFont()
         font.setFamilies(["微软雅黑", "Segoe UI Symbol"])
-        font.setPointSize(11)
-        label.setFont(font)
-        label.setObjectName("Title")
+        self.label.setFont(font)
+        self.label.setObjectName("Title")
 
         self.editor = QPlainTextEdit()
         font = QFont()
-        font.setFamilies(["Source Code Pro"])
-        font.setPointSize(11)
+        font.setFamilies(["Consolas"])
         self.editor.setFont(font)
         self.editor.setObjectName("Editor")
 
         button_group = QHBoxLayout()
-        save_btn = QPushButton("Save")
-        reset_btn = QPushButton("Reset")
+        self.save_btn = QPushButton("Save")
+        self.reset_btn = QPushButton("Reset")
         button_group.addSpacerItem(QSpacerItem(0, 0, QSizePolicy.Expanding))
-        button_group.addWidget(save_btn)
-        button_group.addWidget(reset_btn)
+        button_group.addWidget(self.save_btn)
+        button_group.addWidget(self.reset_btn)
         font = QFont()
         font.setFamilies(["微软雅黑", "Segoe UI Symbol"])
-        font.setPointSize(11)
-        save_btn.setObjectName("SaveButton")
-        save_btn.setCursor(QCursor(Qt.PointingHandCursor))
-        reset_btn.setObjectName("ResetButton")
-        reset_btn.setCursor(QCursor(Qt.PointingHandCursor))
-        save_btn.setFixedHeight(30)
+        self.save_btn.setObjectName("SaveButton")
+        self.save_btn.setCursor(QCursor(Qt.PointingHandCursor))
+        self.reset_btn.setObjectName("ResetButton")
+        self.reset_btn.setCursor(QCursor(Qt.PointingHandCursor))
 
-        save_btn.setFont(font)
-        reset_btn.setFixedHeight(30)
-        reset_btn.setFont(font)
-        vly.addWidget(label)
-        vly.addWidget(self.editor)
-        vly.addLayout(button_group)
+        self.save_btn.setFont(font)
+        self.reset_btn.setFont(font)
+        self.vly.addWidget(self.label)
+        self.vly.addWidget(self.editor)
+        self.vly.addLayout(button_group)
         self.load_setting()
-        save_btn.clicked.connect(self.save_setting)
-        reset_btn.clicked.connect(self.load_setting)
+        self.save_btn.clicked.connect(self.save_setting)
+        self.reset_btn.clicked.connect(self.load_setting)
 
     def load_setting(self):
         with open(self.plugin.setting_path, encoding="utf-8") as setting_file:
@@ -73,32 +67,63 @@ class PluginEditorWidget(QWidget):
             setting_file.write(self.editor.toPlainText())
         self.plugin.reload()
 
+    def change_size(self):
+        s = self.api.size_scale
+        self.vly.setContentsMargins(10 * s.g, 10 * s.g, 0, 10 * s.g)
+
+        font = self.label.font()
+        font.setPixelSize(16 * s.f)
+        self.label.setFont(font)
+        self.label.setFixedHeight(48 * s.g)
+
+        font = self.editor.font()
+        font.setPixelSize(16 * s.f)
+        self.editor.setFont(font)
+
+        font = self.save_btn.font()
+        font.setPixelSize(16 * s.f)
+        self.save_btn.setFont(font)
+        self.reset_btn.setFont(font)
+        self.save_btn.setFixedHeight(30 * s.g)
+        self.reset_btn.setFixedHeight(30 * s.g)
+
 
 class PluginTitleWidget(QWidget):
-    def __init__(self, plugin: AbstractPlugin):
+    def __init__(self, plugin: AbstractPlugin, api: ContextApi):
         super(QWidget, self).__init__()
         self.plugin = plugin
+        self.api = api
         hly = QHBoxLayout()
+        hly.setContentsMargins(0, 0, 0, 0)
 
-        icon_label = QLabel()
+        self.icon_label = QLabel()
         pm = QPixmap(os.path.join(plugin.meta_info.path, plugin.meta_info.icon))
         pm = pm.scaled(32, 32, Qt.IgnoreAspectRatio, Qt.SmoothTransformation)
-        icon_label.setPixmap(pm)
+        self.icon_label.setPixmap(pm)
 
-        name_label = QLabel(plugin.meta_info.name)
+        self.name_label = QLabel(plugin.meta_info.name)
         font = QFont()
         font.setFamilies(["微软雅黑", "Segoe UI Symbol"])
-        font.setPointSize(11)
-        name_label.setFont(font)
-        name_label.setObjectName("Name")
-        name_label.setSizePolicy(QSizePolicy.Expanding, QSizePolicy.Expanding)
-        name_label.setAlignment(Qt.AlignVCenter)
+        font.setPixelSize(16)
+        self.name_label.setFont(font)
+        self.name_label.setObjectName("Name")
+        self.name_label.setSizePolicy(QSizePolicy.Expanding, QSizePolicy.Expanding)
+        self.name_label.setAlignment(Qt.AlignVCenter)
 
-        hly.addWidget(icon_label)
-        hly.addWidget(name_label)
+        hly.addWidget(self.icon_label)
+        hly.addWidget(self.name_label)
 
         self.setLayout(hly)
         self.setCursor(QCursor(Qt.PointingHandCursor))
+
+    def change_size(self):
+        pm = QPixmap(os.path.join(self.plugin.meta_info.path, self.plugin.meta_info.icon))
+        pm = pm.scaled(32 * self.api.size_scale.g, 32 * self.api.size_scale.g, Qt.IgnoreAspectRatio,
+                       Qt.SmoothTransformation)
+        self.icon_label.setPixmap(pm)
+        font = self.name_label.font()
+        font.setPixelSize(16 * self.api.size_scale.f)
+        self.name_label.setFont(font)
 
 
 class SettingDialog(QWidget):
@@ -107,7 +132,9 @@ class SettingDialog(QWidget):
         self.plugin_info = plugin_info
         self.api = api
 
-        self.adjust_frame()
+        self.setWindowTitle("Setting")
+        self.setWindowIcon(QIcon(os.path.join(self.plugin_info.path, "images/setting_icon.png")))
+        self.setWindowFlag(QtCore.Qt.WindowContextHelpButtonHint, False)
 
         self.title_list = QListWidget()
         self.editor_list = QListWidget()
@@ -115,18 +142,6 @@ class SettingDialog(QWidget):
         self.title_list.setObjectName("TitleList")
         self.editor_list.setObjectName("EditorList")
         self.setObjectName("SettingPanel")
-
-    def adjust_frame(self):
-        self.setWindowTitle("Setting")
-        self.setWindowIcon(QIcon(os.path.join(self.plugin_info.path, "images/setting_icon.png")))
-        self.setWindowFlag(QtCore.Qt.WindowContextHelpButtonHint, False)
-
-        self.setFixedWidth(800)
-        self.setFixedHeight(600)
-        qr = self.frameGeometry()
-        cp = QDesktopWidget().availableGeometry().center()
-        qr.moveCenter(cp)
-        self.move(qr.topLeft())
 
     def show(self) -> None:
         with open(os.path.join(self.plugin_info.path, "resource", "theme.css"), "r",
@@ -144,6 +159,27 @@ class SettingDialog(QWidget):
                                                             btbc=cur_theme["color"]))
         if not self.inited:
             self.init_widgets()
+
+        screen = QApplication.desktop().screenNumber(QApplication.desktop().cursor().pos())
+
+        self.setFixedWidth(800 * self.api.size_scale.g)
+        self.setFixedHeight(600 * self.api.size_scale.g)
+        qr = self.frameGeometry()
+        cp = QApplication.desktop().screenGeometry(screen).center()
+        qr.moveCenter(cp)
+        self.move(qr.topLeft())
+
+        self.title_list.setFixedWidth(150 * self.api.size_scale.g)
+        for ii in range(self.title_list.count()):
+            plugin_title_item = self.title_list.item(ii)
+            plugin_title_item.setSizeHint(QSize(0, 48 * self.api.size_scale.g))
+            self.title_list.itemWidget(plugin_title_item).change_size()
+
+        for ii in range(self.editor_list.count()):
+            plugin_editor_item = self.editor_list.item(ii)
+            plugin_editor_item.setSizeHint(QSize(0, 600 * self.api.size_scale.g))
+            self.editor_list.itemWidget(plugin_editor_item).change_size()
+
         super().show()
 
     def init_widgets(self):
@@ -154,23 +190,16 @@ class SettingDialog(QWidget):
         hly.addWidget(self.editor_list)
         hly.setSpacing(0)
 
-        self.title_list.setFixedWidth(150)
-
-        font = QFont()
-        font.setFamilies(["微软雅黑", "Segoe UI Symbol"])
-        font.setPointSize(11)
         for plugin in self.api.setting_plugins:
             plugin_title_item = QListWidgetItem()
-            plugin_title_widget = PluginTitleWidget(plugin)
+            plugin_title_widget = PluginTitleWidget(plugin, self.api)
             self.title_list.addItem(plugin_title_item)
             self.title_list.setItemWidget(plugin_title_item, plugin_title_widget)
-            plugin_title_item.setSizeHint(QSize(0, 48))
 
             plugin_editor_item = QListWidgetItem()
-            plugin_editor_widget = PluginEditorWidget(plugin)
+            plugin_editor_widget = PluginEditorWidget(plugin, self.api)
             self.editor_list.addItem(plugin_editor_item)
             self.editor_list.setItemWidget(plugin_editor_item, plugin_editor_widget)
-            plugin_editor_item.setSizeHint(QSize(0, 600))
 
         def selected_changed(item):
             self.editor_list.scrollToItem(self.editor_list.item(item.row()))
